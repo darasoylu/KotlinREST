7e1d81ddd8099be1894cd006dd65e2dbc41a2aae
DaraSoylu <darasoylu21@gmail.com>
2023-08-21 13:26:18 +0300

    ui change and add screenshots

--- app/src/main/java/com/darasoylu/kotlinrest/util/Constants.kt
+++ app/src/main/java/com/darasoylu/kotlinrest/util/Constants.kt
@@ -2,5 +2,6 @@ package com.darasoylu.kotlinrest.util
 
 object Constants {
 
-    const val BASE_URL = "http://192.168.1.148:5000/" //5000den onceki kisim IPv4 ile degistir
+    const val BASE_URL = "http://10.22.52.34:5000/" //Change the part before 5000 with IPv4
+
 }
